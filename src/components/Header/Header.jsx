7c0571c1d63a98ba63e715a8919a1ec0cf4045c8
Davut Dagtekin <dagtekindavut07@gmail.com>
2025-02-20 15:59:48 +0300

    setup some files, made the header section

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,15 @@
+import "./Header.css";
+
+
+export default function Header() {
+    return(
+    <>
+    <div id="header">
+        <h1>CV Maker</h1>
+        <button id="inspectBtn" type="button"><i className="fa fa-search"></i></button>
+    </div>
+
+    </>
+    )
+}
+
