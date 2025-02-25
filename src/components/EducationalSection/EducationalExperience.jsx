5021026e4e9ce3ee5945aa5eee9aa29bcde70ed5
Davut Dagtekin <dagtekindavut07@gmail.com>
2025-02-25 19:34:02 +0300

    Added education section

---
+++ src/components/EducationalSection/EducationalExperience.jsx
@@ -0,0 +1,60 @@
+import { useState } from "react"
+import "./Education.css"
+import EducationalInfo from "./EducationalInfo";
+
+
+
+function EducationalExperience () {
+    
+    const [educationInfo, setEducationInfo] = useState([{
+        id: crypto.randomUUID(),
+        schoolName: "",
+        titleOfStudy: "",
+        dateOfStudyStart: "",
+        dateOfStudyEnd: ""
+    }]);
+    
+    function AddEducation(){
+      const newArray = [...educationInfo];
+      newArray.push(
+        {
+          id: crypto.randomUUID(),
+          schoolName: "",
+          titleOfStudy: "",
+          dateOfStudyStart: "",
+          dateOfStudyEnd: ""
+      }
+      );
+      setEducationInfo(newArray);
+
+    }
+
+    return (
+    <>
+        <div className="inputSection" id="educationInputSection">
+        <h2 className="informationHeader">Educational Information</h2>
+        <hr />
+        <button id="addEducationBtn" onClick={AddEducation}>
+          <i className="fa fa-plus"></i>
+          </button>
+        {educationInfo.length === 0 ? (
+            <p>click plus to add a segment</p>
+          ) : (
+            educationInfo.map((education, index) => {
+              return (
+                <EducationalInfo
+                key={education.id}
+                educationObject={education}
+                educationInfo={educationInfo}
+                setEducationInfo={setEducationInfo}
+                />
+              );
+            })
+          )}
+            
+        </div>
+    </>
+    )
+}
+
+export default EducationalExperience
---
+++ src/components/EducationalSection/EducationalInfo.jsx
@@ -0,0 +1,59 @@
+import { useState } from "react"
+import "./Education.css"
+
+
+function EducationalInfo ({educationObject, EducationInfo, setEducationInfo}) {
+    
+    const [info, setinfo] = useState(educationObject);
+    
+
+    return (
+    <>
+        <div className="educationBox">
+            <div className="inputRow">
+            <input 
+                name="nameOfSchool"
+                className="inputText"
+                placeholder="School Name Here"
+                type="text"
+                value={info.schoolName}
+                onChange={(event) => setinfo({ ...info, schoolName: event.target.value })}
+            />
+            </div>
+            <div className="inputRow">
+            <input 
+                name="titleOfStudy"
+                className="inputText"
+                placeholder="Title of Study"
+                type="text"
+                value={info.titleOfStudy}
+                onChange={(event) => setinfo({ ...info, titleOfStudy: event.target.value })}
+            />
+            </div>
+            <div className="inputRow">
+            <input 
+                name="studyStartDate"
+                className="inputText"
+                placeholder="Start Date"
+                type="text"
+                value={info.dateOfStudyStart}
+                onChange={(event) => setinfo({ ...info, dateOfStudyStart: event.target.value })}
+            />
+            </div>
+            <div className="inputRow">
+            <input 
+                name="studyEndDate"
+                className="inputText"
+                placeholder="End Date"
+                type="text"
+                value={info.dateOfStudyEnd}
+                onChange={(event) => setinfo({ ...info, dateOfStudyEnd: event.target.value })}
+            />
+            </div>
+        </div>
+        <hr />
+    </>
+    )
+}
+
+export default EducationalInfo
