ce23aea4933f1d43937af5d0512529aa739f1adc
Davut Dagtekin <dagtekindavut07@gmail.com>
2025-03-05 14:58:35 +0300

    added work section

--- src/components/CVSheet/CvSheet.jsx
+++ src/components/CVSheet/CvSheet.jsx
@@ -30,10 +30,28 @@ function CvSheet({generalInfo, workInfo, educationInfo}){
             <div className="cvEducationItem">
                 <p>{education.titleOfStudy}: &nbsp;  <span className="cvDate">{education.dateOfStudyStart} --- {education.dateOfStudyEnd}</span></p>
                 <p><strong>{education.schoolName}</strong></p>
-                <p></p>
-                <p></p>
             </div>
-            <hr className="hrStriped"/>
+            
+            </>
+            )})}
+            </div>
+        </div>
+        <hr className="cvHrLong"/>
+        <div id="cvWork">
+            <div>
+            <h2>Work Experience</h2>
+            </div>
+            <div>
+            {workInfo.map((work)=>{
+            return (
+            <>
+            
+            <div className="cvWorkItem">
+                <p>{work.positionTitle}: &nbsp;  <span className="cvDate">{work.dateOfWorkStart} --- {work.dateOfWorkEnd}</span></p>
+                <p><strong>{work.companyName}</strong></p>
+                <p>{work.mainResponsibilities}</p>
+            </div>
+            
             </>
             )})}
             </div>
